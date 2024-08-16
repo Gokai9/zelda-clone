a78997d630d70c5e782a4de6f21f31687f81ee81
Gokai9 <tasim.dev@gmail.com>
2024-08-16 16:44:55 +0700

    add movement to player

--- code/level.py
+++ code/level.py
@@ -1,4 +1,6 @@
+from typing import Iterable
 import pygame
+from pygame.sprite import AbstractGroup
 from code.settings import WORLD_MAP, TILESIZE
 from code.player import Player
 from code.tile import Tile
@@ -7,7 +9,7 @@ class Level:
     def __init__(self) -> None:
         self.display_surface = pygame.display.get_surface()
         self.visible_sprites = pygame.sprite.Group()
-        self.invisible_sprites = pygame.sprite.Group()
+        self.obstacles_sprites = pygame.sprite.Group()
         self.create_map()
         
     def create_map(self) -> None:
@@ -16,9 +18,15 @@ class Level:
                 x = x_i * TILESIZE
                 y = y_i * TILESIZE
                 if y_arr == "x":
-                    Tile((x, y), [self.visible_sprites, self.invisible_sprites])
+                    Tile((x, y), [self.visible_sprites, self.obstacles_sprites])
                 if y_arr == "p":
-                    Player((x, y), [self.visible_sprites, self.invisible_sprites])
+                    Player((x, y), [self.visible_sprites], self.obstacles_sprites)
 
     def run(self) -> None:
         self.visible_sprites.draw(self.display_surface)
+        self.visible_sprites.update()
+
+
+class YCamera(pygame.sprite.Group):
+    def __init__() -> None:
+        super().__init__()
--- code/player.py
+++ code/player.py
@@ -1,7 +1,56 @@
 import pygame
+from code.debug import debug
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self, pos, *groups) -> None:
-        super().__init__(*groups)
-        self.image_player = pygame.image.load('../resources/graphics/test/player.png').convert_alpha()
-        self.rect = self.image_player.get_rect(topleft = pos)
+    def __init__(self, pos, groups, sprite) -> None:
+        super().__init__(groups)
+        self.image = pygame.image.load('./resources/graphics/test/player.png').convert_alpha()
+        self.rect = self.image.get_rect(topleft = pos)
+        self.direction = pygame.math.Vector2()
+        self.speed = 4
+        self.obstacles_sprites = sprite
+
+    def input_key(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_UP]:
+            self.direction.y = 1
+        elif keys[pygame.K_DOWN]:
+            self.direction.y = -1
+        else:
+            self.direction.y = 0
+        if keys[pygame.K_RIGHT]:
+            self.direction.x = 1
+        elif keys[pygame.K_LEFT]:
+            self.direction.x = -1
+        else:
+            self.direction.x = 0
+
+    def move(self, speed):
+        # if both x, y value not == 0
+        if self.direction.magnitude() != 0:
+            self.direction = self.direction.normalize()
+        self.rect.y -= self.direction.y * speed
+        self.collission_detector('vertical')
+        self.rect.x += self.direction.x * speed
+        self.collission_detector('horizontal')
+
+    def collission_detector(self, direction):
+        if direction == 'vertical':
+            for sprite in self.obstacles_sprites:
+                if sprite.rect.colliderect(self.rect):
+                    if self.direction.y > 0:
+                        self.rect.top = sprite.rect.bottom
+                    if self.direction.y < 0:
+                        self.rect.bottom = sprite.rect.top
+        if direction == 'horizontal':
+            for sprite in self.obstacles_sprites:
+                if sprite.rect.colliderect(self.rect):
+                    if self.direction.x > 0:
+                        self.rect.right = sprite.rect.left
+                    if self.direction.x < 0:
+                        self.rect.left = sprite.rect.right
+
+    def update(self):
+        debug(self.direction.xy)
+        self.input_key()
+        self.move(self.speed)
